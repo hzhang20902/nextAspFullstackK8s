14f1c6df1f336662571858234308cb18b1c7c854
Henry Zhang <hzhang@iworkscorp.com>
2024-03-23 01:55:22 -0400

    psql container running connected yaml file

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b7005662d8c3793b682a13d7141d2a47f70ec5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f41eec491993d6520ccfeb6eeb8e8bd9607746f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
