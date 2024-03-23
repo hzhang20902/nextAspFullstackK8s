f41eec491993d6520ccfeb6eeb8e8bd9607746f1
Henry Zhang <hzhang@iworkscorp.com>
2024-03-23 01:19:03 -0400

    dbcontext and migrations created

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,10 +1,18 @@
 //program execution in order:
 
 //creating the builder
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+//dependency injection to make use of the Db service for classes to get access to the DbContext 
+builder.Services.AddDbContext<AuctionDbContext>(opt => 
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 //actually building
 var app = builder.Build();
--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6942d8b66a5c7d57c0ccee126c351dd512ae500")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b7005662d8c3793b682a13d7141d2a47f70ec5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
