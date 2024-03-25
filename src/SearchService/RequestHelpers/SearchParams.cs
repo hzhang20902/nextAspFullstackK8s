bf4f4a646271795769006271ee54e675769f0df6
Henry Zhang <hzhang@iworkscorp.com>
2024-03-25 18:33:42 -0400

    search filter plus typo ALL TIMES ARE IN THE PAST so < is actually >

--- src/SearchService/RequestHelpers/SearchParams.cs
+++ src/SearchService/RequestHelpers/SearchParams.cs
@@ -7,7 +7,7 @@ namespace SearchService.RequestHelpers
 {
     public class SearchParams
     {
-        public string searchTerm {get;set;}
+        public string SearchTerm {get;set;}
         public int PageNumber {get;set;} = 1;
         public int PageSize {get;set;} = 4;
         public string Seller {get;set;}
